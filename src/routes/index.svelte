062e0e4b95f7315efe319a7d7cada358be4f4f49
Kevin Dangoor <kevindangoor@khanacademy.org>
2021-11-02 21:54:37 -0400

    Commit first files

---
+++ src/routes/index.svelte
@@ -0,0 +1,9 @@
+<svelte:head>
+    <title>Big Climate Impact</title>
+</svelte:head>
+
+<h1><i>You</i> can save the future.</h1>
+<p>This <a href="choosing">non-partisan site for American citizens</a>
+is all about making the best <a href="strategy">strategic</a> choices
+to address the climate emergency <i>and</i> set up America for
+the <a href="economics">biggest economic opportunity in decades</a>.</p>
